9fbadaf8efeebb3007ed6fa1a0296e451b3d858a
Fatih Doğru <f.dogru_pardus@yahoo.com>
2015-02-04 19:46:28 +0200

    added open weather api network snippet and refactored

--- app/src/main/java/com/example/wethil/sunshine/MainActivity.java
+++ app/src/main/java/com/example/wethil/sunshine/MainActivity.java
@@ -1,23 +1,12 @@
 package com.example.wethil.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
+@SuppressWarnings("ALL")
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -26,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -57,43 +46,5 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] forecastArray = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(), // The current context (this activity)
-                    R.layout.list_item_forecast, // The name of the layout ID.
-                    R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                    weekForecast);
-
-            ListView listView = (ListView)  rootView.findViewById(
-                    R.id.listview_forecast
-            );
-
-            listView.setAdapter(mForecastAdapter);
-
-            
-
-            return rootView;
-        }
-    }
 }
