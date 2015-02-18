44940c56250afcbb33c5ac13e42690cb2555e4ce
Fatih Doğru <f.dogru_pardus@yahoo.com>
2015-02-18 11:21:46 +0200

    It can get weather data from cloud and show toast message

--- app/src/main/java/com/example/wethil/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/wethil/sunshine/ForecastFragment.java
@@ -11,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -95,6 +97,13 @@ public class ForecastFragment extends Fragment {
         );
 
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
